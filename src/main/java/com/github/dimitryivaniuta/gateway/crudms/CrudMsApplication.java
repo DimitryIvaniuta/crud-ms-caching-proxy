4f2d71e264abca834ba39b0d99d58ee11e3972e2
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-07 20:01:53 +0100

    Crud ms caching application

--- src/main/java/com/github/dimitryivaniuta/gateway/crudms/CrudMsApplication.java
+++ src/main/java/com/github/dimitryivaniuta/gateway/crudms/CrudMsApplication.java
@@ -1,4 +1,11 @@
 package com.github.dimitryivaniuta.gateway.crudms;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.github.dimitryivaniuta.gateway.crudms")
 public class CrudMsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrudMsApplication.class, args);
+    }
 }
